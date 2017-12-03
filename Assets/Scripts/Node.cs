5d43a6dfb0262826e216528b2d4719882fe7c4bc
Telahnus <quan.jordan@gmail.com>
2017-12-02 20:36:02 -0400

    starting graph nodes, tile prefabs, folder structure

---
+++ Assets/Scripts/Node.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node {
+
+	// private variables
+	private int xCoordinate, zCoordinate, ID;
+	private Node[] connections = new Node[4]; //[N,E,S,W]
+
+	// private methods
+	private void addConnection(int direction, Node node){
+		this.connections[direction] = node;
+	}
+
+	// public methods
+
+	// Constructor
+	public Node(int x, int z, int ID){
+		this.xCoordinate = x;
+		this.zCoordinate = z;
+		this.ID = ID;
+	}
+
+}
