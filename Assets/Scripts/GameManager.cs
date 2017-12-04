ae2dfb6e2746f2d29ab39c09811f1ed488c6b4f9
Telahnus <quan.jordan@gmail.com>
2017-12-04 12:42:40 -0400

    unselected GM by mistake

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -9,17 +9,11 @@ public class GameManager : MonoBehaviour {
     public GameObject currentTile;
 	private List<Tile> graph;
 	public int tileCount = 0;
+	private TileMaker tileMaker;
 	
 	// PREFABS
-	// to group a bunch of properties in the inspector, set as [System.Serializable]
-	// then wrap in a public class, then instantiate that class
 	[System.Serializable]
 	public class Prefabs {
-		public GameObject fourway;
-        public GameObject straight;
-        public GameObject tjunction;
-        public GameObject lcorner;
-        public GameObject blanktile;
         public GameObject cop;
         public GameObject robber;
     } public Prefabs prefabs = new Prefabs();
@@ -38,34 +32,18 @@ public class GameManager : MonoBehaviour {
 		return newObject;
 	}
 
-	private GameObject createTile(string type){
-		GameObject newTile;
-		switch(type){
-			case "fourway": newTile = createObject(prefabs.fourway); break;
-			case "tjunction": newTile = createObject(prefabs.tjunction); break;
-			case "lcorner": newTile = createObject(prefabs.lcorner); break;
-			case "straight": newTile = createObject(prefabs.straight); break;
-			case "blank":
-			default: 
-				newTile = createObject(prefabs.blanktile); 
-				newTile.transform.Rotate(0, Random.Range(0, 4) * 90, 0); 
-				break;
-		}
-		newTile.transform.parent = this.refs.tile_folder.transform;
-        Tile scriptTile = newTile.AddComponent<Tile>();
-        scriptTile.constructor(0, 0, tileCount++, type);
-		return newTile;
-	}
-
 	/// Start is called on the frame when a script is enabled just before
 	/// any of the Update methods is called the first time.
 	void Start(){
-		player = createObject(prefabs.cop); player.name = "player";
-		currentTile = createTile("fourway"); currentTile.name = "currentTile";
-		createTile("blank").GetComponent<Tile>().setLocation(0, 1);
-		createTile("blank").GetComponent<Tile>().setLocation(0, -1);
-		createTile("blank").GetComponent<Tile>().setLocation(-1, 0);
-		createTile("blank").GetComponent<Tile>().setLocation(1, 0);
+    	tileMaker = this.GetComponent<TileMaker>();
+
+        player = createObject(prefabs.cop); player.name = "player";
+		currentTile = tileMaker.createTile("fourway"); currentTile.name = "currentTile";
+
+		tileMaker.createTile("blank").GetComponent<Tile>().setLocation(0, 1);
+		tileMaker.createTile("blank").GetComponent<Tile>().setLocation(0, -1);
+		tileMaker.createTile("blank").GetComponent<Tile>().setLocation(-1, 0);
+		tileMaker.createTile("blank").GetComponent<Tile>().setLocation(1, 0);
 	}
 
 	/// Update is called every frame, if the MonoBehaviour is enabled.
